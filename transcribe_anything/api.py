e1a19f20da8f5055432eb54ec3ce4c8650eb4d79
zackees <z@zackees.com>
2022-11-30 15:20:39 -0800

    improve naming of output folder to pass tests

--- transcribe_anything/api.py
+++ transcribe_anything/api.py
@@ -20,21 +20,27 @@ PERMS = (
 )
 
 
-def transcribe(url_or_file: str) -> None:
-    """
-    Runs the whole program on the input resource and writes
-      * out_file if not None
-      * or prints to sys.stdout
-    """
+def sanitize_folder_name(folder_name: str) -> str:
+    """Sanitize a folder name."""
+    folder_name = folder_name.split("?")[0]
+    return "".join([c for c in folder_name if c.isalnum() or c in ["-", "_"]])
 
+
+def transcribe(url_or_file: str, output_dirname: str | None = None) -> None:
+    """
+    Runs the program.
+    """
     basename = os.path.basename(url_or_file)
-    dirname = os.path.splitext(basename)[0].split("?")[0]
-    os.makedirs(dirname, exist_ok=True)
-    tmp_mp3 = os.path.join(dirname, "out.mp3")
+    if output_dirname is None:
+        output_dirname = sanitize_folder_name(basename)
+        # sanitize dirname
+        output_dirname = "".join([c for c in output_dirname if c.isalnum() or c in ["-", "_"]])
+    os.makedirs(output_dirname, exist_ok=True)
+    tmp_mp3 = os.path.join(output_dirname, "out.mp3")
     try:
         fetch_audio(url_or_file, tmp_mp3)
         assert os.path.exists(tmp_mp3), f"Path {tmp_mp3} doesn't exist."
-        cmd = f"whisper {tmp_mp3} --output_dir {dirname}"
+        cmd = f"whisper {tmp_mp3} --output_dir {output_dirname}"
         sys.stderr.write(f"Running:\n  {cmd}\n")
         # proc = CapturingProcess(cmd, stdout=StringIO(), stderr=StringIO())
         proc = subprocess.Popen(cmd, shell=True)  # pylint: disable=consider-using-with
