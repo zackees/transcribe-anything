8bb939b3f685dcfc6c26484b17b66aedaa54a082
zackees <z@zackees.com>
2022-12-01 03:23:17 -0800

    clean up

--- setup.py
+++ setup.py
@@ -80,12 +80,7 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Environment :: Console",
     ],
-    install_requires=REQUIREMENTS + [
-        'whisper @ git+https://github.com/zackees/whisper.git#egg=whisper'
-        'yt-dlp',
-        'static-ffmpeg',
-        'torch'
-    ],
+    install_requires=REQUIREMENTS,
     entry_points = {
         'console_scripts': [
             'transcribe_anything = transcribe_anything.cmd:main',
