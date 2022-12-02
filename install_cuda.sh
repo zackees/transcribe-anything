69d82733a82d6ab7dba9ae344539df33e2d94458
zackees <z@zackees.com>
2022-12-02 15:51:27 -0800

    adds cuda install script

---
+++ install_cuda.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+pip uninstall -y torch torchvision torchaudio
+pip cache purge
+
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
+pip install -e .
+
+echo "transcribe audio is installed, run it with transcribe_audio <URL OR FILE>"
