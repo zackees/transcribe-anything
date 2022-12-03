547cde8993f648daeb85751079bcac6c84c3a90a
zackees <z@zackees.com>
2022-12-02 18:11:09 -0800

    fix install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -2,9 +2,14 @@
 
 set -e
 pip uninstall -y torch torchvision torchaudio
+
+echo "Purging pip cache to remove any torch packages that are cpu only"
 pip cache purge
 
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
-pip install -e .
+echo "Installing torch+cuda"
+pip install "torch<1.13.0" --extra-index-url https://download.pytorch.org/whl/cu116
+
+echo "Install transcribe_anything"
+pip install .
 
 echo "transcribe audio is installed, run it with transcribe_audio <URL OR FILE>"
