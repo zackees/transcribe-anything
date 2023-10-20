aedb1aac1e4bfe8ebe110d1bd6b02fc1cd6da319
zackees <z@zackees.com>
2023-10-19 17:12:00 -0700

    update linting

--- lint.sh
+++ lint.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 set -e
-echo running pylint...
-pylint transcribe_anything tests install_cuda.py
+
+echo running ruff...
+ruff transcribe_anything tests install_cuda.py
 
 echo running flake8...
 flake8 transcribe_anything tests install_cuda.py
+
+echo running pylint...
+pylint transcribe_anything tests install_cuda.py
 
 echo running mypy...
 mypy transcribe_anything tests install_cuda.py
