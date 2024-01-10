57aafe8a29587fde31734972ccbdcca04fc6753a
zackees <z@zackees.com>
2024-01-10 14:45:45 -0800

    update whisper into it's own functions. Start work on insanely fast whisper

---
+++ tests/test_insanely_fast_whisper.py
@@ -0,0 +1,43 @@
+"""
+Tests transcribe_anything
+"""
+
+# pylint: disable=bad-option-value,useless-option-value,no-self-use,protected-access,R0801
+# flake8: noqa E501
+
+import os
+import unittest
+import shutil
+from pathlib import Path
+
+from transcribe_anything.whisper import run_insanely_fast_whisper
+
+HERE = Path(os.path.abspath(os.path.dirname(__file__)))
+LOCALFILE_DIR = HERE / "localfile"
+TESTS_DATA_DIR = LOCALFILE_DIR / "text_video" / "en"
+TEST_WAV = LOCALFILE_DIR  / "video.wav"
+
+
+class InsanelFastWhisperTester(unittest.TestCase):
+    """Tester for transcribe anything."""
+
+    @unittest.skip("DISABLED FOR NOW - WORK IN PROGRESS")
+    def test_local_file(self) -> None:
+        """Check that the command works on a local file."""
+        shutil.rmtree(TESTS_DATA_DIR, ignore_errors=True)
+        run_insanely_fast_whisper(
+            input_wav=TEST_WAV,
+            #device="cuda",
+            #device="cpu",
+            device="cuda:0",
+            model="small",
+            output_dir=TESTS_DATA_DIR,
+            task="transcribe",
+            language="en",
+            other_args=None,
+        )
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- transcribe_anything/whisper.py
+++ transcribe_anything/whisper.py
@@ -11,6 +11,59 @@ import subprocess
 from typing import Optional
 
 
+def get_current_python_version() -> str:
+    """Returns the current python version."""
+    return f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+
+# for insanely fast whisper, use:
+#   pipx install insanely-fast-whisper --python python3.11
+
+
+def run_insanely_fast_whisper(  # pylint: disable=too-many-arguments
+    input_wav: Path,
+    device: str,  # pylint: disable=unused-argument
+    model: str,
+    output_dir: Path,
+    task: str,
+    language: str,
+    other_args: Optional[list[str]]
+) -> None:
+    """Runs insanely fast whisper."""
+    cmd_list = []
+    model = f"openai/whisper-{model}"
+    if sys.platform == "win32":
+        # Set the text mode to UTF-8 on Windows.
+        cmd_list.extend(["chcp", "65001", "&&"])
+    cmd_list += [
+        "insanely-fast-whisper",
+        "--file-name", str(input_wav),
+        "--device-id", "0",
+        "--model-name", model,
+        "--task", task,
+        "--language", language,
+        "--transcript-path", str(output_dir),
+    ]
+    if other_args:
+        cmd_list.extend(other_args)
+    # Remove the empty strings.
+    cmd_list = [x.strip() for x in cmd_list if x.strip()]
+    cmd = " ".join(cmd_list)
+    sys.stderr.write(f"Running:\n  {cmd}\n")
+    proc = subprocess.Popen(  # pylint: disable=consider-using-with
+        cmd, shell=True, universal_newlines=True,
+        encoding="utf-8"
+    )
+    while True:
+        rtn = proc.poll()
+        if rtn is None:
+            time.sleep(0.25)
+            continue
+        if rtn != 0:
+            msg = f"Failed to execute {cmd}\n "
+            raise OSError(msg)
+        break
+
+
 def run_whisper(  # pylint: disable=too-many-arguments
     input_wav: Path,
     device: str,
