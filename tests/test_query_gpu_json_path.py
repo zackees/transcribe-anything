32f60b9c931187a91fc120299a87c82311820295
zackees <z@zackees.com>
2024-02-10 19:08:41 -0800

    adds missing file

---
+++ tests/test_query_gpu_json_path.py
@@ -0,0 +1,37 @@
+"""
+Tests transcribe_anything
+"""
+
+# pylint: disable=bad-option-value,useless-option-value,no-self-use,protected-access,R0801
+# flake8: noqa E501
+
+import json
+import os
+import tempfile
+import unittest
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+class BadVideoTitleTester(unittest.TestCase):
+    """Tester for transcribe anything."""
+
+    def test_local_file(self) -> None:
+        """Check that the command works on a local file."""
+        prevdir = os.getcwd()
+        with tempfile.TemporaryDirectory() as tmpdir:
+            try:
+                print(f"Using temporary directory {tmpdir}")
+                os.chdir(tmpdir)
+                os.system("transcribe_anything --query-gpu-json-path info.json")
+                self.assertTrue(os.path.exists("info.json"))
+                # parsing test
+                with open("info.json", encoding="utf-8", mode="rt") as fd:
+                    info = json.load(fd)
+                self.assertTrue(info is not None)
+            finally:
+                os.chdir(prevdir)
+
+
+if __name__ == "__main__":
+    unittest.main()
