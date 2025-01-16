5d58012a90a3c40475db299871de98ad3e4caf1a
zackees <z@zackees.com>
2025-01-16 00:13:23 -0800

    update

--- install.sh
+++
@@ -1,7 +0,0 @@
-# Warning: Out of date.
-cd $( dirname ${BASH_SOURCE[0]})
-python ./make_venv.py
-. ./activate.sh
-python -m pip install pip --upgrade
-python -m pip install -r requirements.testing.txt
-python -m pip install -e .
